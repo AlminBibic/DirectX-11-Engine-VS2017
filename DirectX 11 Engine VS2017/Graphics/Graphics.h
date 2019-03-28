cad1bf4ef3186ade7d1328757d685848bd1d9a72
Almin <34369753+Teppar@users.noreply.github.com>
2019-03-28 15:22:24 +0100

    Texturing

--- DirectX 11 Engine VS2017/Graphics/Graphics.h
+++ DirectX 11 Engine VS2017/Graphics/Graphics.h
@@ -4,6 +4,7 @@
 #include "Vertex.h"
 #include <SpriteBatch.h>
 #include <SpriteFont.h>
+#include <WICTextureLoader.h>
 
 
 class Graphics
@@ -26,7 +27,6 @@ private:
 	PixelShader pixelshader;
 
 	Microsoft::WRL::ComPtr<ID3D11Buffer> vertexBuffer;
-	Microsoft::WRL::ComPtr<ID3D11Buffer> vertexBuffer2;
 
 	Microsoft::WRL::ComPtr<ID3D11DepthStencilView> depthStencilView;
 	Microsoft::WRL::ComPtr<ID3D11Texture2D> depthStencilBuffer;
@@ -37,4 +37,7 @@ private:
 
 	std::unique_ptr<DirectX::SpriteBatch> spriteBatch;
 	std::unique_ptr<DirectX::SpriteFont> spriteFont;
+
+	Microsoft::WRL::ComPtr<ID3D11SamplerState> samplerState;
+	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> myTexture;
 };
--- DirectX 11 Engine VS2017/Source.cpp
+++ DirectX 11 Engine VS2017/Source.cpp
@@ -1,17 +1,27 @@
 #include "Engine.h"
 
+
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPWSTR    lpCmdLine,
 	_In_ int       nCmdShow)
 {
-	Engine engine;
-	engine.Initialize(hInstance, "DirectXEngine", "MyWindowClass", 800, 600);
-	while (engine.ProcessMessages() == true)
+
+	HRESULT hr = CoInitialize(NULL);
+	if (FAILED(hr))
 	{
-		engine.Update();
-		engine.RenderFrame();
+		ErrorLogger::Log(hr, "Failed to call CoInitialize.");
+		return -1;
 	}
 
+	Engine engine;
+	if (engine.Initialize(hInstance, "Title", "MyWindowClass", 800, 600))
+	{
+		while (engine.ProcessMessages() == true)
+		{
+			engine.Update();
+			engine.RenderFrame();
+		}
+	}
 	return 0;
 }
